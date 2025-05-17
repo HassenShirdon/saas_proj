3a87bbbb86a6cdedbca9ec50dabb603a396410b9
HassenShirdon <hassen.hared@gmail.com>
2025-05-17 17:02:25 +0200

    fixed the admin register site

--- backend/inventory/admin.py
+++ backend/inventory/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Supplier, ProductCategory,             InventoryItem, PurchaseOrder, PurchaseOrderItem, Customer,     SalesOrder, SalesOrderItem, StockMovement
+
+
+admin.site.register(Supplier)
+admin.site.register(ProductCategory)
+admin.site.register(InventoryItem)
+admin.site.register(PurchaseOrder)
+admin.site.register(PurchaseOrderItem)
+admin.site.register(Customer)
+admin.site.register(SalesOrder)
+admin.site.register(SalesOrderItem)
+admin.site.register(StockMovement)    
